a396fec6d4d4d9fdb945504f31bb8fca959a0b19
nikhilphotos0712 <Nikhilphotos0712@gmail.com>
2022-05-26 20:09:57 +0530

    removed toogle

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,7 +4,7 @@ const Nav = (props) => {
         <div>
             <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Word Analyzer</a>
+                    <a className="navbar-brand" href="/#">Word Analyzer</a>
                     <div className={`form-check form-switch text-${props.mode==="light"?"grey":"light"}`}>
                         <input className="form-check-input" onClick={props.togglemode} type="checkbox" id="flexSwitchCheckDefault" />
                         <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Dark Mode</label>
